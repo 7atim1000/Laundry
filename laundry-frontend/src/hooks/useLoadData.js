074d75b39e2455e0976ae68758bf0016b9be0832
Hatim <7atim1000@gmail.com>
2025-08-17 21:30:10 +0400

    Update useLoadData.js

--- laundry-frontend/src/hooks/useLoadData.js
+++ laundry-frontend/src/hooks/useLoadData.js
@@ -12,28 +12,31 @@ const useLoadData = () => {
     const [ isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
-
         const fetchUser = async () => {
-        try {
-           const { data } = await getUserData();
-           console.log(data);
-
-           const {_id, name, email, phone, role } = data.data;
-           dispatch(setUser({ _id, name, email, phone, role }))
-
-        } catch (error) {
-            dispatch(removeUser());
-            navigate('/auth')
-            console.log(error)
-        } finally {
-            setIsLoading(false);
+            try {
+                const { data } = await getUserData();
+                
+                // Check if the response is actually successful
+                if (data?.success) {
+                    const {_id, name, email, phone, role } = data.data;
+                    dispatch(setUser({ _id, name, email, phone, role }));
+                } else {
+                    throw new Error("No user data");
+                }
+            } catch (error) {
+                // Clear any potential lingering cookies
+                document.cookie = 'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                dispatch(removeUser());
+                navigate('/auth');
+            } finally {
+                setIsLoading(false);
+            }
         }
-    }
 
-    fetchUser();
+        fetchUser();
+    }, [dispatch, navigate]);
 
-   }, [dispatch, navigate]);
-   return isLoading;
+    return isLoading;
 }
 
 
